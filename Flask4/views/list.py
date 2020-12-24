731ac35b2b52ce8300a75bd2fe48efd8e4888b58
nsaedae@gmail.com <nsaedae@gmail.com>
2020-12-24 17:42:23 +0900

    2020/12/24 수업예제

--- Flask4/views/list.py
+++ Flask4/views/list.py
@@ -1,7 +1,40 @@
 from flask import Blueprint, render_template
+import pymysql as db
 
 list_blueprint = Blueprint('list', __name__)
 
 @list_blueprint.route('/list')
 def list():
-    return render_template('/list.html')
+    # 1단계
+    conn = db.connect(host='192.168.10.114',
+                      user='chhak',
+                      password='1234',
+                      db='chhak',
+                      charset='utf8')
+    # 2단계
+    cursor = conn.cursor()
+
+    # 3단계
+    sql = "SELECT * FROM `MEMBER`;"
+    cursor.execute(sql)
+
+    # 4단계
+    members = []
+
+    for row in cursor.fetchall():
+        member = {
+            'uid': row[0],
+            'name': row[1],
+            'hp': row[2],
+            'pos': row[3],
+            'dep': row[4],
+            'rdate': row[5]
+        }
+
+        members.append(member)
+
+
+    # 5단계
+    conn.close()
+
+    return render_template('/list.html', members=members)
